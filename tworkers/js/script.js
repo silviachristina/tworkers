adda5ebc3d99692830d8611d0f973c01758b262f
silviachristina <silvia.christina@hotmail.com>
2018-04-13 13:30:07 -0300

    Add files via upload

---
+++ tworkers/js/script.js
@@ -0,0 +1,6 @@
+// /*---------------------------------------
+//   AUDIO SETTINGS
+//   --------------------------------------- */
+
+//   var audio = document.getElementById("bgmusic");
+//   audio.volume = 0.00; //ajustar o volume antes de alterar as configuracoes
---
+++ tworkers/servico.php
@@ -0,0 +1,11 @@
+<!-- Mais utilizada - Recomendado  -->
+<?php 
+echo "Seja bem vindão ao php.";
+?> 
+
+<!-- Não é necessário o fechamento da Tag, usado para não correr o risco de codigo ficar fora e ser enviado para o cliente -->
+<?php 
+echo "Seja bem vindo ao php.";
+
+// //Forma padrão declaração de linguagens
+// <script language="php"></script>
